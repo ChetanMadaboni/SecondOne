8127bfd2818282de8d1164dd96d0d377adb67365
chetanMadaboni <chetanmadabony19@gmail.com>
2020-07-15 18:38:56 +0530

    Add new demo check.

---
+++ demo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 05:39:34 2020
+
+@author: HP
+"""
+
+if __name__ == '__main__':
+    n=input()
+    r=[]
+    for i in n[len(n)::-1]:
+        r.append(i)
+    st="".join(str(x) for x in r)
+    print(st)    
+        
